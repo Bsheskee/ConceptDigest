bd96688d4508ccd6840a50355598d59eb523d7b1
Bsheskee <bartlomiejbialobrzewski@gmail.com>
2023-11-12 16:08:18 +0100

    Initial Commit

---
+++ ConceptDigest/ConceptDigestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConceptDigestApp.swift
+//  ConceptDigest
+//
+//  Created by bartek on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConceptDigestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
